3c3d77f0ea283bdd81594ef2aeee9be74b2393e7
Stephane Vaillancourt <svaillancourt@gmail.com>
2020-05-22 13:26:31 -0600

    js updates

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+// Grab the index list of all todos.
+const allToDos = document.getElementById( 'index' );
+// If available, proceed.
+if ( allToDos )
+{ // Run Axios to obtain list.
+  axios.get( 'https://localhost:44390/api/ToDoItems/' )
+    // Get just the data from the response.
+    .then( response => response.data )
+    // "Process" our data (JSON object or array.)
+    .then( data => {
+      console.log( data );
+      // Output list of todos.
+      // data.forEach( toDo => {
+
+      // } );
+    } );
+}
